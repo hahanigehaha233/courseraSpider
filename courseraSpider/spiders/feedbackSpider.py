904260cbe27819a5bd5a48e627751710f405d1ef
wangpan <wangpan233@gmail.com>
2018-04-27 19:00:14 +0800

    Fix Bug re aboout content

--- courseraSpider/spiders/feedbackSpider.py
+++ courseraSpider/spiders/feedbackSpider.py
@@ -5,35 +5,37 @@ import json
 import re
 from courseraSpider.pipelines import FeedbackSpiderPipeline
 from courseraSpider.items import FeedbackSpiderItem
-
+#encoding=utf-8
 class feedbackSpider(scrapy.Spider):
     name = "getcoursefeedback"
     start_urls = []
     url = "https://www.coursera.org/api/feedback.v1/?q=course&courseId="
-    para = "&feedbackSystem=STAR&ratingValues=1%2C2%2C3%2C4%2C5&categories=generic&start=0&limit=100"
+    para = "&feedbackSystem=STAR&ratingValues=1%2C2%2C3%2C4%2C5&categories=generic&start=0&limit=20"
     dict = FeedbackSpiderPipeline.get_project_url()
     for i in dict:
         start_urls.append(str(url+FeedbackSpiderPipeline.init_start_urls(i[0])[0][0]+para))
 
     def parse(self, response):
-        json_data = json.loads(response._body)
-        for data in json_data['elements']:
-            item = FeedbackSpiderItem()
-            item['content'] = re.search(r'<text>.*</text>', data['comments']['generic']['definition']['value']).group()[6:-7]
-            tmp = item['content']
-            tmp = tmp.replace('<text>', '')
-            tmp = tmp.replace(r'</text>', '')
-            item['content'] = tmp
-            item['userid'] = re.search('^\d+', data['id']).group()
-            item['timestamp'] = data['timestamp']
-            item['rating'] = data['rating']['value']
-            item['table_name'] = re.search('/\w*$', FeedbackSpiderPipeline.find_name_from_id(str(data['context']['definition']['courseId']))[0][0].replace('-', '_')).group()[1::]
-            yield item
-        if(json_data['paging']['next']):
-            bits = list(urlparse.urlparse(response.url))
-            qs = urlparse.parse_qs(bits[4])
-            qs['start'][0] = str(int(qs['limit'][0]) + int(qs['start'][0]) + 1)
-            bits[4] = urllib.urlencode(qs, True)
-            r_url = urlparse.urlunparse(bits)
-            yield scrapy.Request(r_url)
+            json_data = json.loads(response._body)
+            for data in json_data['elements']:
+                item = FeedbackSpiderItem()
+                tmp = data['comments']['generic']['definition']['value']
+                tmp = tmp.replace('<text>', '')
+                tmp = tmp.replace('</text>', '')
+                tmp = tmp.replace('<co-content>', '')
+                tmp = tmp.replace('</co-content>', '')
+                item['content'] = tmp
+                item['userid'] = re.search('^\d+', data['id']).group()
+                item['timestamp'] = data['timestamp']
+                item['rating'] = data['rating']['value']
+                item['table_name'] = re.search('/\w*$', FeedbackSpiderPipeline.find_name_from_id(str(data['context']['definition']['courseId']))[0][0].replace('-', '_')).group()[1::]
+                yield item
+            if(json_data['paging']['next']):
+                bits = list(urlparse.urlparse(response.url))
+                qs = urlparse.parse_qs(bits[4])
+                qs['start'][0] = str(int(qs['limit'][0]) + int(qs['start'][0]))
+                bits[4] = urllib.urlencode(qs, True)
+                r_url = urlparse.urlunparse(bits)
+                yield scrapy.Request(r_url)
+
 
